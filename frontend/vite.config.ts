c0f27c50c35ebff7736b449371f22c2ccdb4c523
hugopuybareau <hugo.puybareau@gmail.com>
2025-07-13 16:09:06 +0200

    fix(port): 8080 now

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+// frontend/vite.config.ts
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { componentTagger } from 'lovable-tagger';
+
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    server: {
+        host: '::',
+        port: 8080,
     },
-  },
+    plugins: [react(), mode === 'development' && componentTagger()].filter(
+        Boolean
+    ),
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 }));
